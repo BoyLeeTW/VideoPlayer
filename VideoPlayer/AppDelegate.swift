69e97c048962b051b4a9af4bbc56c48681f1a694
BoyLeeTW <boy0726@gmail.com>
2017-09-06 14:50:12 +0800

    set up video search bar lay out

--- VideoPlayer/AppDelegate.swift
+++ VideoPlayer/AppDelegate.swift
@@ -16,9 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
+
         let homeViewController = VideoPlayerViewController()
-        homeViewController.view.backgroundColor = UIColor.red
+
+        homeViewController.view.backgroundColor = UIColor.black
+
         window!.rootViewController = homeViewController
+
         self.window?.makeKeyAndVisible()
 
         return true
--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class VideoPlayerViewController: UIViewController {
 
+    let videoSearchBar = UISearchBar()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setUpVideoSearchingTextField()
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setUpVideoSearchingTextField() {
+
+        self.view.addSubview(videoSearchBar)
+
+        videoSearchBar.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 27).isActive = true
+
+        videoSearchBar.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+
+        videoSearchBar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height * 30.0 / 667.0).isActive = true
+
+        videoSearchBar.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 359.0 / 370.0 ).isActive = true
+
+        videoSearchBar.translatesAutoresizingMaskIntoConstraints = false
+
+//        videoSearchBar.barTintColor = .clear
+
+        videoSearchBar.layer.cornerRadius = 5
+
+        videoSearchBar.placeholder = "Enter URL of video"
+
     }
 
 }
