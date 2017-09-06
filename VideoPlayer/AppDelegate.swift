1f2979cc1f8ecbf1fe1d013c17db60c2bff78c6c
BoyLeeTW <boy0726@gmail.com>
2017-09-06 14:08:29 +0800

    add app icon

--- VideoPlayer/AppDelegate.swift
+++ VideoPlayer/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
-        let homeViewController = UIViewController()
+        let homeViewController = VideoPlayerViewController()
         homeViewController.view.backgroundColor = UIColor.red
         window!.rootViewController = homeViewController
         self.window?.makeKeyAndVisible()
